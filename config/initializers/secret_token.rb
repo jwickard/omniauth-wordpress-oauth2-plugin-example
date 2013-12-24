a0a2d36a00ea2c3b03c72e8a2a89d09e423afe36
Joel Wickard <jwickard@gmail.com>
2013-12-24 11:30:21 -0600

    Initial Rails Application Skeleton.  Sqlite backend.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthWordpressOauth2PluginExample::Application.config.secret_key_base = 'a75720891a3095837721ef23bfc86631968e2483decf1abdbf2cd6c3768a0bca88028c09ddcc67702800c40e4fefef7389a41b06c970e1e926d5f1eb004ce8c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmniauthWordpressOauth2PluginExample::Application.config.session_store :cookie_store, key: '_omniauth-wordpress-oauth2-plugin-example_session'
